fa6a9ee15f4a13111f67eaf781e3507e266d7de1
Maqsudjon <murodovabasira@gmail.com>
2025-07-02 02:55:51 -0700

    ok

---
+++ movies.py
@@ -0,0 +1,30 @@
+movies = {
+    "1": {
+        "title": "Kalmar o'yini 3chi mavsum 1-qism",
+        "file_id": "BAACAgIAAxkBAAMSaGPP4YvEcCGumJSf4cnawNvFuxcAAvN2AAIhKAFL1Iy0cxekRns2BA"  
+    },
+    "2": {
+        "title": "Kalmar o'yini 3chi mavsum 2-qism",
+        "file_id": "BAACAgIAAxkBAAMUaGPP8WojKki0YF2tnqTWCNU4_aEAAslyAAJ5cyFLd0cODkiUph02BA"  
+    },
+    "3": {
+        "title": "Kalmar o'yini 3chi mavsum 3-qism",
+        "file_id": "BAACAgIAAxkBAAMWaGPQD8uTUDEpvYYHaWfz-cyu5ogAAuNyAAJ5cyFLtDMF2hAB6mA2BA"  
+    },
+    "4": {
+        "title": "Kalmar o'yini 3chi mavsum 4-qism",
+        "file_id": "BAACAgIAAxkBAAMpaGPQi6GEVrbROfVM8MVIZ79lQZMAAgpzAAJ5cyFLCmUUe8f53w82BA"  
+    },
+    "5": {
+        "title": "Kalmar o'yini 3chi mavsum 5-qism",
+        "file_id": "BAACAgIAAxkBAAMraGPQk92-GQ-4h0h3ERcRkUhwZoEAAhFzAAJ5cyFLQr7ljdwLLrs2BA"  
+    },
+    "6": {
+        "title": "Kalmar o'yini 3chi mavsum 6-qism",
+        "file_id": "BAACAgIAAxkBAAMzaGPQwpA4HcC7Mz3hg9aoWVUYQeYAAtlrAAIGewFL-5nn85yMWCo2BA"  
+    },
+    # "7": {
+    #     "title": "",
+    #     "file_id": ""  
+    # },
+}
